cbf3a6eb2cf0fbcf74af068994de4ce743c6c598
Upmostbubble151 <heeresh.ganeshan@education.nsw.gov.au>
2026-06-25 09:54:53 +1000

    Finishing home page
    Introducing a user input for both Username and Password. Creating a def function to both display a background font. Finished the Login Page

--- Main Program.py
+++ Main Program.py
@@ -39,11 +39,38 @@ def home_page(frame):
     canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
     #Widget 
-    user_entry = tk.Entry(frame, width=30, font=("Arial", 14))
-    user_entry.place(x=110, y=500)
+    user_entry = tk.Entry(frame, width=30, bd=0, highlightthickness=0, relief = "flat", bg="white", font=("Arial", 16))
+    user_entry.insert(0, "Enter your username")  # Placeholder text
+    user_entry.place(x=80, y=540)
+    def clear_placeholder(event): #Username Prompt
+        if user_entry.get() == "Enter your username":
+            user_entry.delete(0, tk.END)
+            user_entry.config(fg="black")
+    user_entry.bind("<FocusIn>", clear_placeholder)
 
-    title = tk.Label(frame, text="Home Page", font=("Arial", 24, "bold"))
-    title.place(x=150, y=50)
+
+    def restore_placeholder(event):#Defines the function to restore the placeholder text when the entry field loses focus
+        if user_entry.get() == "":
+            user_entry.insert(0, "Enter your username")
+            user_entry.config(fg="grey")
+    user_entry.bind("<FocusOut>", restore_placeholder)
+    username = user_entry.get()  # Get the username from the entry field
+    print("Username:", username)  # Print the username to the console
+    #Password
+    pass_entry = tk.Entry(frame, width=30, bd=0, highlightthickness=0, relief = "flat", bg="white", font=("Arial", 16), show="*")
+    pass_entry.insert(0, "Enter your password")  # Placeholder text
+    pass_entry.place(x=75, y=660)
+    #Password Prompt
+    def clear_placeholder_pass(event): 
+        if pass_entry.get() == "Enter your password":
+            pass_entry.delete(0, tk.END)
+            pass_entry.config(fg="black", show="*")
+    pass_entry.bind("<FocusIn>", clear_placeholder_pass)
+    #Login Button
+    login_button = tk.Button(frame, text="Login", font=("Arial", 16, "bold"), bg="black", fg="black", bd=0, highlightthickness=0, relief="flat")
+    login_button.place(x=150, y=770)
+
+
 
 page_switch(home_page)  # Display the home page initially
 root.mainloop()
