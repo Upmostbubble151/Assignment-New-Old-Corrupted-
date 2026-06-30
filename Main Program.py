330cc3d3988be36979d6815e18aaf239bdd5060c
Upmostbubble151 <heeresh.ganeshan@education.nsw.gov.au>
2026-06-30 10:57:24 +1000

    Created Q1 Correct Page
    In this log update, I created the Q1 Correct and implemented a working function to keep track of users score.

--- Main Program.py
+++ Main Program.py
@@ -16,6 +16,7 @@ class QuizApp:
         #Switching Pages
         self.current_page = None #Acts as a changable variable which is us
         self.page_switch(self.home_page)  # Display the home page initially
+        self.score = 0 #Intialise the Score to be counted
     #Page Switch
     def page_switch (self, page_function): #Defines Variable that changes to the different Pages. page_function is the page you want to switch
         if self.current_page is not None:
@@ -113,7 +114,7 @@ class QuizApp:
             next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q1))
             next_button.place(x=350, y=475)
     def lecture1_Q1(self, frame):
-            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture 1 - Question 1.png")
+            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture 1 - Q1 .png")
             image = image.resize((430, 932)) # Resize the image to be correct
 
             bg_image = ImageTk.PhotoImage(image) # Store the Image
@@ -124,6 +125,43 @@ class QuizApp:
             canvas.pack(fill="both", expand=True)
             canvas.bg_image = bg_image
             canvas.create_image(0, 0, image=bg_image, anchor="nw")
+            #Provide An entry box for the user to input their answer
+            answer_entry = tk.Entry(frame, font=("Arial", 16), width = 10)
+            answer_entry.place(x=150, y=750)
+            #Function to check the answer and update the score
+            def check_answer():
+                 user_entry = answer_entry.get().strip().upper()  # Converts user input to uppercase and remove spaces
+                 correct_answer = "A"
+
+                 if user_entry == correct_answer:
+                     self.score += 1  # Add Score 1 to variable to keep track
+                     print("Correct! Score:", self.score)
+                     self.page_switch(self.lecture1_Q1_Correct)
+                 else:
+                        print("Incorrect. Score:", self.score)
+                        
+            #Submit Button
+            submit_button = tk.Button(frame, text="Submit", font=("Arial", 16, "bold"), command=check_answer)                 
+            submit_button.place(x=200, y=800)
+                    
+
+    def lecture1_Q1_Correct(self, frame):
+            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture1_Q1_Correct.png")
+            image = image.resize((430, 932)) # Resize the image to be correct
+
+            bg_image = ImageTk.PhotoImage(image) # Store the Image
+            bg_label = tk.Label(frame, image=bg_image)
+
+            #Creates Canvas to display the background image
+            canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+            canvas.pack(fill="both", expand=True)
+            canvas.bg_image = bg_image
+            canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+            next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q2))
+            next_button.place(x=350, y=475)
+
+
 
             
         
