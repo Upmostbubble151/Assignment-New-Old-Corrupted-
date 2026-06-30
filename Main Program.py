5f6dfa6244d14598b73abc0fb9c89bbb4808e175
Upmostbubble151 <heeresh.ganeshan@education.nsw.gov.au>
2026-06-30 21:27:43 +1000

    Finshed Program
    Created the final 3 questions, created the report page with a powerful message to motivate users.

--- Main Program.py
+++ Main Program.py
@@ -344,8 +344,229 @@ class QuizApp:
             canvas.bg_image = bg_image
             canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
-            next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q4))
+            next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q5))
             next_button.place(x=350, y=475)
+    def lecture1_Q4_Incorrect(self, frame):
+            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture1_Q4_Incorrect.png")
+            image = image.resize((430, 932)) # Resize the image to be correct
+
+            bg_image = ImageTk.PhotoImage(image) # Store the Image
+            bg_label = tk.Label(frame, image=bg_image)
+
+            #Creates Canvas to display the background image
+            canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+            canvas.pack(fill="both", expand=True)
+            canvas.bg_image = bg_image
+            canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+            next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q5))
+            next_button.place(x=350, y=475)
+        
+    def lecture1_Q5(self, frame):
+            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture1_Q5.png")
+            image = image.resize((430, 932)) # Resize the image to be correct
+
+            bg_image = ImageTk.PhotoImage(image) # Store the Image
+            bg_label = tk.Label(frame, image=bg_image)
+
+            #Creates Canvas to display the background image
+            canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+            canvas.pack(fill="both", expand=True)
+            canvas.bg_image = bg_image
+            canvas.create_image(0, 0, image=bg_image, anchor="nw")
+            #Provide An entry box for the user to input their answer
+            answer_entry = tk.Entry(frame, font=("Arial", 16), width = 10)            
+            answer_entry.place(x=150, y=750)
+            #Function to check the answer and update the score
+            def check_answer():
+                    user_entry = answer_entry.get().strip().upper()  # Converts user input to uppercase and remove spaces
+                    correct_answer = "B"
+    
+                    if user_entry == correct_answer:
+                        self.score += 1  # Add Score 1 to variable to keep track
+                        print("Correct! Score:", self.score)
+                        self.page_switch(self.lecture1_Q5_Correct)
+                    else:
+                            print("Incorrect. Score:", self.score)
+                            self.page_switch(self.lecture1_Q5_Incorrect)
+            submit_button = tk.Button(frame, text="Submit", font=("Arial", 16, "bold"), command=check_answer)                 
+            submit_button.place(x=200, y=800)
+
+    def lecture1_Q5_Correct(self, frame):
+            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture1_Q5_Correct.png")
+            image = image.resize((430, 932)) # Resize the image to be correct
+
+            bg_image = ImageTk.PhotoImage(image) # Store the Image
+            bg_label = tk.Label(frame, image=bg_image)
+
+            #Creates Canvas to display the background image
+            canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+            canvas.pack(fill="both", expand=True)
+            canvas.bg_image = bg_image
+            canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+            next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q6))
+            next_button.place(x=350, y=475)
+    def lecture1_Q5_Incorrect(self, frame):
+            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture1_Q5_Incorrect.png")
+            image = image.resize((430, 932)) # Resize the image to be correct
+
+            bg_image = ImageTk.PhotoImage(image) # Store the Image
+            bg_label = tk.Label(frame, image=bg_image)
+
+            #Creates Canvas to display the background image
+            canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+            canvas.pack(fill="both", expand=True)
+            canvas.bg_image = bg_image
+            canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+            next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q6))
+            next_button.place(x=350, y=475)
+
+    def lecture1_Q6(self, frame):
+            image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/lecture1_Q6.png")
+            image = image.resize((430, 932)) # Resize the image to be correct
+
+            bg_image = ImageTk.PhotoImage(image) # Store the Image
+            bg_label = tk.Label(frame, image=bg_image)
+
+            #Creates Canvas to display the background image
+            canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+            canvas.pack(fill="both", expand=True)
+            canvas.bg_image = bg_image
+            canvas.create_image(0, 0, image=bg_image, anchor="nw")
+            #Provide An entry box for the user to input their answer
+            answer_entry = tk.Entry(frame, font=("Arial", 16), width = 10)            
+            answer_entry.place(x=150, y=750)
+            #Function to check the answer and update the score
+            def check_answer():
+                    user_entry = answer_entry.get().strip().upper()  # Converts user input to uppercase and remove spaces
+                    correct_answer = "C"
+    
+                    if user_entry == correct_answer:
+                        self.score += 1  # Add Score 1 to variable to keep track
+                        print("Correct! Score:", self.score)
+                        self.page_switch(self.lecture1_Q6_Correct)
+                    else:
+                            print("Incorrect. Score:", self.score)
+                            self.page_switch(self.lecture1_Q6_Incorrect)
+            submit_button = tk.Button(frame, text="Submit", font=("Arial", 16, "bold"), command=check_answer)                 
+            submit_button.place(x=200, y=800)
+
+    def lecture1_Q6_Correct(self, frame):
+        image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/lecture1_Q6_Correct.png")
+        image = image.resize((430, 932))
+
+        bg_image = ImageTk.PhotoImage(image)
+
+        canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+        canvas.pack(fill="both", expand=True)
+        canvas.bg_image = bg_image
+        canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+        next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q7)); next_button.place(x=350, y=475)
+        next_button.place(x=350, y=475)
+        
+    def lecture1_Q6_Incorrect(self, frame):
+        image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/lecture1_Q6_Incorrect.png")
+        image = image.resize((430, 932))
+
+        bg_image = ImageTk.PhotoImage(image)
+        bg_label = tk.Label(frame, image=bg_image)
+
+        canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+        canvas.pack(fill="both", expand=True)
+        canvas.bg_image = bg_image
+        canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+        next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_Q7))
+        next_button.place(x=350, y=475)
+
+    def lecture1_Q7(self, frame):
+        image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture1_Q7.png")
+        image = image.resize((430, 932))
+
+        bg_image = ImageTk.PhotoImage(image)
+        bg_label = tk.Label(frame, image=bg_image)
+
+        canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+        canvas.pack(fill="both", expand=True)
+        canvas.bg_image = bg_image
+        canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+        answer_entry = tk.Entry(frame, font=("Arial", 16), width=10)
+        answer_entry.place(x=150, y=750)
+
+        def check_answer():
+                user_entry = answer_entry.get().strip().upper()
+                correct_answer = "C"
+
+                if user_entry == correct_answer:
+                    self.score += 1
+                    print("Correct! Score:", self.score)
+                    self.page_switch(self.lecture1_Q7_Correct)
+                else:
+                    print("Incorrect. Score:", self.score)
+                    self.page_switch(self.lecture1_Q7_Incorrect)
+
+        submit_button = tk.Button(frame, text="Submit", font=("Arial", 16, "bold"), command=check_answer)
+        submit_button.place(x=200, y=800)
+
+
+    def lecture1_Q7_Correct(self, frame):
+        image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Lecture1_Q7_Correct.png")
+        image = image.resize((430, 932))
+
+        bg_image = ImageTk.PhotoImage(image)
+        bg_label = tk.Label(frame, image=bg_image)
+
+        canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+        canvas.pack(fill="both", expand=True)
+        canvas.bg_image = bg_image
+        canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+        next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_report))
+        next_button.place(x=350, y=475) 
+
+    def lecture1_Q7_Incorrect(self, frame):
+        image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/lecture1_Q7_Incorrect.png")
+        image = image.resize((430, 932))
+
+        bg_image = ImageTk.PhotoImage(image)
+        bg_label = tk.Label(frame, image=bg_image)
+
+        canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+        canvas.pack(fill="both", expand=True)
+        canvas.bg_image = bg_image
+        canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+        next_button = tk.Button(frame, text=">", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.lecture1_report))
+        next_button.place(x=350, y=475)
+    
+    def lecture1_report(self, frame):
+        image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/lecture1_Report.png")
+        image = image.resize((430, 932))
+
+        bg_image = ImageTk.PhotoImage(image)
+
+        canvas = tk.Canvas(frame, width=430, height=932, bd=0, highlightthickness=0)
+        canvas.pack(fill="both", expand=True)
+        canvas.bg_image = bg_image
+        canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
+        #Displaying a positive message of result
+        
+        if self.score == 7:
+                message = f"Excellent! {self.score}/7"
+        elif self.score >= 5:
+                message = f"Great job! {self.score}/7"
+        elif self.score >= 3:
+                message = f"Good effort! {self.score}/7"
+        else:
+                message = f"Keep practising! {self.score}/7"
+
+        message_label = tk.Label(frame, text=message, font=("Arial", 20, "bold"), bg="white", fg="black")
+        message_label.place(x=110, y=600)
 
     def main_menu1(self,frame):
         image = Image.open("/Users/HeereshGaneshan/Documents/VIsual Studio Code/Assignment 2/Assignment New (Old Corrupted)/Main Menu 2 Quizzlet.png")
@@ -362,6 +583,8 @@ class QuizApp:
         back_button = tk.Button(frame, text="<", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.main_menu))
         back_button.place(x=20, y=475)
 
+        trylater_button = tk.Button(frame, text="Try Again Later", bg="#6b63fe", bd=0, highlightthickness=0, activebackground="#ffffff", relief="flat", command=lambda: self.page_switch(self.main_menu1))
+        trylater_button.place(x=170, y=750)
 root = tk.Tk()
 app = QuizApp(root)
 root.mainloop()
